c9cb2c0191fd55986b0707fcf2445ffd6a65d778
hnoraa <aaron.horeth.e@gmail.com>
2022-08-11 22:55:18 -0400

    Updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # pixelToMidi
 Create a midi file from an array of pixels
+
+## TODO
+- [ ] command line args for specifying config file paths
+- [ ] better error handling
+- [ ] tests to see if ALL files exist (not just images)
+- [ ] GUI
--- imageData.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-from numpy import asarray
-
-
-class ImageData():
-    def __init__(self, file_name):
-        self.file_name = file_name
-        self.image = Image.open(self.file_name)
-        self.pixel_array = asarray(self.image)
