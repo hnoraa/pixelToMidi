753e9b238ba006e24f3d62eb299b1b217a5fa68f
hnoraa <aaron.horeth.e@gmail.com>
2022-08-12 14:48:10 -0400

    UI updates

--- ui.py
+++ ui.py
@@ -1,38 +1,66 @@
-from tkinter import Tk, Text, BOTH, W, N, E, S, Canvas, Menu
-from tkinter.ttk import Frame, Button, Label, Style
+from tkinter import *
+from tkinter.ttk import *
 
 
-class Window(Frame):
+class Window(Tk):
     def __init__(self):
         super().__init__()
 
-        self.initUi()
+        self.title('Pixel To MIDI')
+        self.geometry('600x600')
+        self.statusLblTxt = StringVar()
 
-    def initUi(self):
-        self.master.title("Pixel to MIDI")
-        self.pack(fill=BOTH, expand=True)
+        self.__statusBar()
+        self.__menu()
+        self.__tabs()
 
-        self.menubar = Menu(self)
-        fM = Menu(self.menubar, tearoff=0)
-        self.menubar.add_cascade(label="File", menu=fM)
+        self.config(menu=self.mainMenu)
 
-        self.columnconfigure(1, weight=1)
-        self.columnconfigure(3, pad=7)
-        self.rowconfigure(3, weight=1)
-        self.rowconfigure(5, pad=7)
+    def __menu(self):
+        self.mainMenu = Menu(self)
+        fM = Menu(self.mainMenu, tearoff=0)
+        self.mainMenu.add_cascade(label='File', menu=fM)
+        fM.add_command(label='New', command=self.__menuNew)
+        fM.add_command(label='Open', command=self.__menuOpen)
+        fM.add_separator()
+        fM.add_command(label='Save', command=self.__menuSave)
+        fM.add_command(label='Save As', command=self.__menuSaveAs)
+        fM.add_separator()
+        fM.add_command(label='Exit', command=self.destroy)
 
-        area = Canvas(self, bg="white")
-        area.grid(row=1, column=0, columnspan=2, rowspan=4,
-                  padx=5, sticky=E+W+S+N)
+    def __statusBar(self):
+        self.statusBar = Label(
+            self, 
+            textvariable=self.statusLblTxt, 
+            border=1,
+            relief=SUNKEN, 
+            anchor=W
+        )
+        self.statusBar.pack(side=BOTTOM, fill=X)
 
-        abtn = Button(self, text="Activate")
-        abtn.grid(row=1, column=3)
+    def __tabs(self):
+        self.tabControl = Notebook(self)
+        self.tabSong = Frame(self.tabControl)
+        self.tabTracks = Frame(self.tabControl)
+        self.tabImage = Frame(self.tabControl)
+        
+        self.tabControl.add(self.tabSong, text='Song')
+        self.tabControl.add(self.tabTracks, text='Tracks')
+        self.tabControl.add(self.tabImage, text='Image')
+        self.tabControl.pack(expand=1, fill='both')
 
-        cbtn = Button(self, text="Close")
-        cbtn.grid(row=2, column=3, pady=4)
+    def __menuNew(self):
+        self.statusLblTxt.set('New menu selected')
 
-        hbtn = Button(self, text="Help")
-        hbtn.grid(row=5, column=0, padx=5)
+    def __menuOpen(self):
+        self.statusLblTxt.set('Open menu selected')
 
-        obtn = Button(self, text="OK")
-        obtn.grid(row=5, column=3)
+    def __menuSave(self):
+        self.statusLblTxt.set('Save menu selected')
+
+    def __menuSaveAs(self):
+        self.statusLblTxt.set('Save As menu selected')
+
+if __name__ == '__main__':
+    win = Window()
+    win.mainloop()
